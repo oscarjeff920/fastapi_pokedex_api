12b31728a906ad78661a38677cd5539a84738c6f
Oscar Jeffery <oscarjjeff920@gmail.com>
2022-02-03 11:59:12 -0300

    I added the try and except statements for request.get, .json() and pydantic class validataion errors alongside pytest tests for each respective case

--- fastapi_pokedex_api/pokeapi_requests.py
+++ fastapi_pokedex_api/pokeapi_requests.py
@@ -1,7 +1,7 @@
 import fastapi
+import pydantic.error_wrappers
 import requests
 from pydantic import typing
-from requests import Session
 
 from fastapi_pokedex_api.configs.pokeapi_config import PokeapiSettings
 from fastapi_pokedex_api.pokedex_schema import PokedexBase
@@ -11,19 +11,29 @@ def get_pokedata(ip_settings: PokeapiSettings, poke_name: str) -> PokedexBase:
     name = poke_name.lower()
     try:
         response = requests.get("{}{}".format(ip_settings.POKEAPI_IP, name))
-        response.raise_for_status()
-    except fastapi.HTTPException as httperr:
-        raise httperr
+    except requests.exceptions.HTTPError:
+        raise fastapi.HTTPException(
+            status_code=404, detail="pokemon {} not found. Check spelling".format(name)
+        )
 
-    details = response.json()
-    return PokedexBase(
-        name=name,
-        description=english_description(details.get("flavor_text_entries", {})),
-        habitat=details.get("habitat").get("name")
-        if details.get("habitat") is not None
-        else None,
-        is_legendary=details.get("is_legendary"),
-    )
+    try:
+        details = response.json()
+    except requests.exceptions.JSONDecodeError:
+        raise fastapi.HTTPException(status_code=406, detail="response not jsonable")
+
+    try:
+        return PokedexBase(
+            name=name,
+            description=english_description(details.get("flavor_text_entries", {})),
+            habitat=details.get("habitat").get("name")
+            if details.get("habitat") is not None
+            else None,
+            is_legendary=details.get("is_legendary"),
+        )
+    except pydantic.error_wrappers.ValidationError:
+        raise fastapi.HTTPException(
+            status_code=406, detail="received json does not have required keys"
+        )
 
 
 def english_description(descriptions: typing.List) -> typing.Optional[str]:
